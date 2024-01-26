b22cb16e90fb47e11b1195c18750a2ca5484aad0
sampath ayyalasomayajula <54360250+abhi05062000@users.noreply.github.com>
2024-01-26 10:58:48 -0800

    Update pattern-1.py

--- Data Structures/Patterns/pattern-1.py
+++ Data Structures/Patterns/pattern-1.py
@@ -1,1 +1,14 @@
+# This is pattern 1
+# Input: N = 5
+# Output:
+# * * * * * *
+# * * * * * *
+# * * * * * *
+# * * * * * *
+# * * * * * *
 
+n = int(input("Enter the value:"))
+for i in range(n):
+  for j in range(n):
+     print("*",end=" ")
+  print("")
